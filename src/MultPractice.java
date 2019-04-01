c41162eb1d6121e6605fad744f4d615e7b411876
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-04-01 08:32:07 -0400

    Update MultPractice.java

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,25 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice
+{
+    private int first;
+    private int second;
+    
+    public MultPractice(int first, int second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getProblem()
+    {
+        String problem = first + "TIMES" + second;
+        return problem;
+    }
+
+    public void nextProblem()
+    {
+        second += 1;
+    }
 }
